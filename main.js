3a87dac4f487a13273ce1b0ef6a6e35170f9c2b6
Dastu6 <antoine.delenclos37@gmail.com>
2023-12-20 17:53:40 +0100

    New javascript function to check if user is afk

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    check_afk();
+}
+
+var check_afk = function() {
+    var lastTimeEvent;
+    window.onload = resetAfkTimer();
+    document.onmousedown = resetAfkTimer();
+    document.onkeydown = resetAfkTimer();
+    document.onclick = resetAfkTimer();
+
+    function logout() {
+        alert("Vous êtes déconnectés.");
+    }
+
+    function resetAfkTimer() {
+        clearTimeout(lastTimeEvent);
+        lastTimeEvent = setTimeout(logout, 3000);
+    }
+}
