c1a37ae8ee65c50d2f7694f53f355faa9d726a00
Dastu6 <antoine.delenclos37@gmail.com>
2023-12-21 11:26:12 +0100

    test check idle

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //var intervalId = window.setInterval(check_afk,1000);
-window.onload(check_afk())
+/*window.onload(check_afk())
 var check_afk = function() {
     var lastTimeEvent;
     //window.onload = resetAfkTimer();
@@ -17,4 +17,23 @@ var check_afk = function() {
         clearTimeout(lastTimeEvent);
         lastTimeEvent = setTimeout(logout, 3000);
     }
-}
+}*/
+
+var inactivityTime = function () {
+    var time;
+    window.onload = resetTimer;
+    // DOM Events
+    document.onmousemove = resetTimer;
+    document.onkeydown = resetTimer;
+
+    function logout() {
+        alert("You are now logged out.")
+        //location.href = 'logout.html'
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, 3000)
+        // 1000 milliseconds = 1 second
+    }
+};
