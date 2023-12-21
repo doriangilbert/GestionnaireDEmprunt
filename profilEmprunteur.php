00ff986001fe91ff0f3a5d90ae74ea68631a9439
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2023-12-21 12:07:15 +0100

    Ajout du profil emprunteur

---
+++ profilEmprunteur.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GestionnaireDEmprunt</title>
+    <link rel="stylesheet" href="ressources/styles/style.css">
+    <link href="ressources/styles/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="h-100 text-black">
+<div class="d-flex justify-content-center align-items-center h-100 flex-column">
+    <h1 class="mb-5">Profil emprunteur</h1>
+    <h3 class="mb-5">Matricule : XXXXX</h3>
+    <a href="profilEmprunteur.php" class="btn btn-primary m-5 p-4">Faire un nouvel emprunt de matériel</a>
+    <a href="index.php" class="btn btn-primary m-5 p-4">Se déconnecter</a>
+</div>
+
+<footer class="fixed-bottom">
+    <div class="row bg-primary text-white">
+        <span class="col-4"></span>
+        <p class="col-4 text-center mb-1 mt-1">GestionnaireDEmprunt</p>
+    </div>
+</footer>
+</body>
+
+</html>
