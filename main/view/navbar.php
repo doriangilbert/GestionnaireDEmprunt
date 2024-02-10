38803a77cfcebf8bbf375ca4bb35c360a487efc5
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2024-02-10 12:39:39 +0100

    update Navbar

--- main/view/navbar.php
+++ main/view/navbar.php
@@ -1,74 +1,30 @@
 <?php
 session_start();
+if (isset($_SESSION["matricule"])) {
+    if ($_SESSION['isAdmin'])
+        $url = "profilAdmin.php";
+    else
+        $url = "profilEmprunteur.php";
+}
+else
+    $url = "login.php";
 ?>
 
 <header>
     <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
         <div class="container-fluid">
 
-            <a class="navbar-brand" href="/">Logo</a>
+            <a class="navbar-brand fw-bold" href="index.php">Gestionnaire d'emprunts</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarColor03">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/">Accueil
-                            <span class="visually-hidden">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">A propos</a>
-                    </li>
 
-                    <li class="nav-item dropdown" >
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" >Documents</a>
-
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Articles</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Rapports</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Prototypes</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Compte-Rendus</a>
-
-                        </div>
-
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">News</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Nouvelles</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Séminaires</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Réunions</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Evénements</a>
-
-                        </div>
-
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Vidéos</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Demos</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Capture</a>
-
-                        </div>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Membres</a>
-                    </li>
-                </ul>
-            </div>
             <div class="navbar-item-custom">
-                <a href="login.php">Connexion</a> | <a href="#">null</a>
+                <div class="d-flex align-items-center">
+                    <a href="index.php" class="text-black text-decoration-none px-3">Accueil</a>
+                    <a class="text-black text-decoration-none px-3" href=<?php echo $url ?>>Mon compte</a>
+                </div>
             </div>
         </div>
     </nav>
