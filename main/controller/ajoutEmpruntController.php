740f586150be4e544ea2425d60763e6f951cee4d
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2024-02-11 19:58:29 +0100

    Ajout de vérifications pour l'ajout d'emprunts

--- main/controller/ajoutEmpruntController.php
+++ main/controller/ajoutEmpruntController.php
@@ -10,7 +10,13 @@ $dateDebut = $_POST['inputDateDebut'];
 $dateFin = $_POST['inputDateFin'];
 $matricule = $_SESSION['matricule'];
 
-$emprunt = new Emprunt($reference, $dateDebut, $dateFin, $matricule);
+try {
+    $emprunt = new Emprunt($reference, $dateDebut, $dateFin, $matricule);
+} catch (Exception $e) {
+    $_SESSION['alert_message'] = $e->getMessage();
+    header('Location: ../view/ajoutEmprunt.php');
+    exit();
+}
 
 $_SESSION['alert_message'] = "Emprunt ajouté avec succès";
 
--- main/entity/Emprunt.php
+++ main/entity/Emprunt.php
@@ -16,6 +16,18 @@ class Emprunt
         $this->dateDebut = $dateDebut;
         $this->dateFin = $dateFin;
         $this->matricule = $matricule;
+
+        if ($dateDebut > $dateFin) {
+            throw new Exception("La date de fin doit être postérieure à la date de début");
+        }
+
+        $result = mysqli_query(BD_Link::connexion(), "SELECT * FROM emprunte WHERE reference = '$reference'") or die("Erreur BD : Select emprunt");
+        while ($ligne_result = mysqli_fetch_assoc($result)) {
+            if (($dateDebut >= $ligne_result['Date_debut'] && $dateDebut <= $ligne_result['Date_de_fin']) || ($dateFin >= $ligne_result['Date_debut'] && $dateFin <= $ligne_result['Date_de_fin'])) {
+                throw new Exception("Un emprunt est déjà en cours pour ce matériel");
+            }
+        }
+
         mysqli_query(BD_Link::connexion(), "INSERT INTO emprunte (reference, date_debut, date_de_fin, matricule) VALUES ('$this->reference', '$this->dateDebut', '$this->dateFin', '$this->matricule')") or die("Erreur BD : Insert emprunt");
     }
 }
